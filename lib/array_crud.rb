c4d624124c204d4239fed5650abdbd1f411f5e55
Chelsey <cswilik@gmail.com>
2020-10-09 01:18:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["cats", "dogs", "chickens"]
-  array.push ["cows"]
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push ["arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
